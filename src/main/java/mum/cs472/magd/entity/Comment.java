6b362ecc5673bb1628c530003d45e5f2d0bc2c0f
Daniyal Saeed <daniyal.sd@gmail.com>
2017-05-20 11:35:49 -0500

    Update Comment.java

--- src/main/java/mum/cs472/magd/entity/Comment.java
+++ src/main/java/mum/cs472/magd/entity/Comment.java
@@ -1,5 +1,126 @@
 package mum.cs472.magd.entity;
 
-public class Comment {
+import java.io.Serializable;
+import java.util.Date;
 
+public final class Comment implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	
+	private Integer commentId;
+	private Post postId;
+	private String commentText;
+	private Date dateCreated;
+	private Date dateUpdated;
+	
+	public Comment(){}
+
+	public Comment(Integer commentId, Post postId, String commentText, Date dateCreated, Date dateUpdated) {
+		super();
+		this.commentId = commentId;
+		this.postId = postId;
+		this.commentText = commentText;
+		this.dateCreated = dateCreated;
+		this.dateUpdated = dateUpdated;
+	}
+
+	public Integer getCommentId() {
+		return commentId;
+	}
+
+	public void setCommentId(Integer commentId) {
+		this.commentId = commentId;
+	}
+
+	public Post getPostId() {
+		return postId;
+	}
+
+	public void setPostId(Post postId) {
+		this.postId = postId;
+	}
+
+	public String getCommentText() {
+		return commentText;
+	}
+
+	public void setCommentText(String commentText) {
+		this.commentText = commentText;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public Date getDateUpdated() {
+		return dateUpdated;
+	}
+
+	public void setDateUpdated(Date dateUpdated) {
+		this.dateUpdated = dateUpdated;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((commentId == null) ? 0 : commentId.hashCode());
+		result = prime * result + ((commentText == null) ? 0 : commentText.hashCode());
+		result = prime * result + ((dateCreated == null) ? 0 : dateCreated.hashCode());
+		result = prime * result + ((dateUpdated == null) ? 0 : dateUpdated.hashCode());
+		result = prime * result + ((postId == null) ? 0 : postId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comment other = (Comment) obj;
+		if (commentId == null) {
+			if (other.commentId != null)
+				return false;
+		} else if (!commentId.equals(other.commentId))
+			return false;
+		if (commentText == null) {
+			if (other.commentText != null)
+				return false;
+		} else if (!commentText.equals(other.commentText))
+			return false;
+		if (dateCreated == null) {
+			if (other.dateCreated != null)
+				return false;
+		} else if (!dateCreated.equals(other.dateCreated))
+			return false;
+		if (dateUpdated == null) {
+			if (other.dateUpdated != null)
+				return false;
+		} else if (!dateUpdated.equals(other.dateUpdated))
+			return false;
+		if (postId == null) {
+			if (other.postId != null)
+				return false;
+		} else if (!postId.equals(other.postId))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Comment [commentId=" + commentId + ", postId=" + postId + ", commentText=" + commentText
+				+ ", dateCreated=" + dateCreated + ", dateUpdated=" + dateUpdated + "]";
+	}
+	
+	
 }
+
