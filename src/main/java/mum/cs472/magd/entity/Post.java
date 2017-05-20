4a1c23e3f4600729c1b77298148e9b2643ef7c6a
Daniyal Saeed <daniyal.sd@gmail.com>
2017-05-20 11:36:10 -0500

    Update Post.java

--- src/main/java/mum/cs472/magd/entity/Post.java
+++ src/main/java/mum/cs472/magd/entity/Post.java
@@ -1,5 +1,129 @@
 package mum.cs472.magd.entity;
 
-public class Post {
+import java.io.Serializable;
+import java.util.Date;
 
+public final class Post implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Integer postId;
+	private User userId;
+	private String postText;
+	private Date dateCreated;
+	private Date dateUpdated;
+	
+	public Post(){}
+	
+	public Post(Integer postId, User userId, String postText, Date dateCreated, Date dateUpdated) {
+		super();
+		this.postId = postId;
+		this.userId = userId;
+		this.postText = postText;
+		this.dateCreated = dateCreated;
+		this.dateUpdated = dateUpdated;
+	}
+
+	public Integer getPostId() {
+		return postId;
+	}
+
+	public void setPostId(Integer postId) {
+		this.postId = postId;
+	}
+
+	public User getUserId() {
+		return userId;
+	}
+
+	public void setUserId(User userId) {
+		this.userId = userId;
+	}
+
+	public String getPostText() {
+		return postText;
+	}
+
+	public void setPostText(String postText) {
+		this.postText = postText;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public Date getDateUpdated() {
+		return dateUpdated;
+	}
+
+	public void setDateUpdated(Date dateUpdated) {
+		this.dateUpdated = dateUpdated;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dateCreated == null) ? 0 : dateCreated.hashCode());
+		result = prime * result + ((dateUpdated == null) ? 0 : dateUpdated.hashCode());
+		result = prime * result + ((postId == null) ? 0 : postId.hashCode());
+		result = prime * result + ((postText == null) ? 0 : postText.hashCode());
+		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Post other = (Post) obj;
+		if (dateCreated == null) {
+			if (other.dateCreated != null)
+				return false;
+		} else if (!dateCreated.equals(other.dateCreated))
+			return false;
+		if (dateUpdated == null) {
+			if (other.dateUpdated != null)
+				return false;
+		} else if (!dateUpdated.equals(other.dateUpdated))
+			return false;
+		if (postId == null) {
+			if (other.postId != null)
+				return false;
+		} else if (!postId.equals(other.postId))
+			return false;
+		if (postText == null) {
+			if (other.postText != null)
+				return false;
+		} else if (!postText.equals(other.postText))
+			return false;
+		if (userId == null) {
+			if (other.userId != null)
+				return false;
+		} else if (!userId.equals(other.userId))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Post [postId=" + postId + ", userId=" + userId + ", postText=" + postText + ", dateCreated="
+				+ dateCreated + ", dateUpdated=" + dateUpdated + "]";
+	}
+	
+	
+	
+	
 }
+
